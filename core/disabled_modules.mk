3cf6478dc6f3bf0ea6308fa43b34d9a2f6aa8db9
Marco Chen <mchen@mozilla.com>
2013-05-02 17:21:51 +0800

    Add to support build.sh (all).

---
+++ core/disabled_modules.mk
@@ -0,0 +1,21 @@
+DISABLED_USER_MODULES := \
+	system_server \
+	app_process \
+	surfaceflinger \
+	libsystem_server \
+	libandroid_servers \
+	libsurfaceflinger \
+	libtestplugin \
+	temp_layoutlib \
+	libvideoeditor_jni \
+	libmedia_jni \
+	libmock_ril \
+	libI420colorconvert \
+	apkcheck \
+	elftree \
+	jdwpspy \
+	a3dconvert \
+	libsampleplugin \
+	app_process__asan \
+	librs_jni \
+
